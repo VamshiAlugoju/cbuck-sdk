e3bfa4b60b6f7aa3d29700586674b806e247dbe1
giftyaustin <austingifty@gmail.com>
2025-07-30 11:24:37 +0530

    url change

--- webview/src/socketClient.ts
+++ webview/src/socketClient.ts
@@ -17,7 +17,7 @@ class SocketClient {
   }
 
   connect(userId: string) {
-    const socket = this.io("https://275a4827f0e6.ngrok-free.app", {
+    const socket = this.io("https://d465631c4589.ngrok-free.app", {
       query: { userId },
       transports: ["websocket"],
     });
